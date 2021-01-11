79ac4729e0dbbefed7b1b11ab3e3306f256c9cba
dsfloresr <74881154+dsfloresr@users.noreply.github.com>
2021-01-10 23:27:57 -0500

    Update Deber_8.ino

--- Deber_8.ino
+++ Deber_8.ino
@@ -7,56 +7,56 @@
  
 
 
-#include<avr/power.h>
-#include<avr/sleep.h>
-#include<avr/wdt.h>
-#include<EEPROM.h>
+#include<avr/power.h>       //Librerias
+#include<avr/sleep.h>      //Librerias
+#include<avr/wdt.h>       //Librerias
+#include<EEPROM.h>       //Librerias
 
-int dato;
-int dato1;
-int i;
+int dato;            //variable 1
+int i;              //variable 2 
 //reset por lineas de programación
 
 void (* resetFunc)(void)=0;
 void setup(){
-  //EEPROM.write(0,0);
-  dato=EEPROM.read(0);
-  Serial.begin(9600);
-  if(dato==0){
-    sistema();
-    EEPROM.write(1,0);
-    i=EEPROM.read(1);
+  //EEPROM.write(0,0);           Escritura de dato inicial, se realiza una sola vez 
+  dato=EEPROM.read(0);          //Lectura del dato 0, para activacion del sistema completo
+  Serial.begin(9600);          //Velocidad del cx
+  if(dato==0){              //Condicion de activacion de metodo
+    sistema();              //Activacion de metodo  
+    EEPROM.write(1,0);     //Actualizacion dato auxiliar
+    i=EEPROM.read(1);     //Lectura de dato auxiliar
   }else{
-    i=EEPROM.read(1);
-    i=i+1;
-    EEPROM.update(1,i);
+    i=EEPROM.read(1);    //LEctura de dato auxiliar
+    i=i+1;               //Aumento de dato auxiliar
+    EEPROM.update(1,i);  //Actualizacion dato auxiliar
   }
-  if(i<15){
-  delay(100);
-  modo();
+  if(i<15){           //Condicion de activacion del modo sleep (15*4 = 60 segundos)
+  delay(100);        
+  modo();           //Metodo de activacion de modo sleep
   }else{
-    EEPROM.update(0,0);
-    wdt_enable(WDTO_15MS);
+    EEPROM.update(0,0);       //Actualizacion dato principal para el sistema
+    wdt_enable(WDTO_15MS);   //Condicion reinicio del sistema.
   }
 }
   void loop(){     
+   
   }
 void modo(){
-    wdt_enable(WDTO_4S);
-    set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-    sleep_enable();
-    sleep_mode();
-    sleep_disable();
-    wdt_disable();
+    wdt_enable(WDTO_4S);     // modo sleep por 4 segundos
+    set_sleep_mode(SLEEP_MODE_PWR_SAVE);  //Seleccion modo sleep
+    sleep_enable();     //Activacion modo sleep
+    sleep_mode();      //Funcionamiento modo sleep 
+    sleep_disable();   //Desactivar modo sleep 
+    wdt_disable();    //Desactivar perro guiardian
 }
 void sistema(){
-  Serial.println("Iniciando Lectura");
-  Serial.println("El dato obtenido es :");
-  EEPROM.update(0,1);
-  Serial.println(analogRead(0));
-  Serial.println("Volviendo a modo Sleep");
-  delay(100);
+  Serial.println("Iniciando Lectura");    //Mensaje de confirmacion de lectura cada 60 segundos
+  Serial.println("El dato obtenido es :");   //Mensaje de informacion
+  EEPROM.update(0,1);           //Actualizacion dato principal del sistema
+  Serial.println(analogRead(0));      // Lectura del dato mediante puerto analogo
+  Serial.println("Volviendo a modo Sleep");     //Informacion activacion modo sleep
+  delay(100);     
 }
 void encender(){
-  resetFunc();
+  resetFunc();       //Funcion reiniciar arduino
 }
